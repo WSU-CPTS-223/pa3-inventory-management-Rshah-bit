68c2ac0c5e756de22c043f89bc70e60ba9687dbd
Rshah <ryhan.shah@wsu.edu>
2025-10-27 13:53:14 -0700

    Market Data; start of hpp; test upload

---
+++ inventorylist.hpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+struct inventorylist
+{ // basic idea at a glance i'll start with
+    /* data */
+    string UniqId;
+    string ProductName;
+    string BrandName;
+    string Asin;
+    string Category;
+    string UpcEanCode;
+    string ListPrice;
+    string SellingPrice;
+    string Quantity;
+    string ModelNumber;
+    string AboutProduct;
+    string ProductSpecification;
+    string TechnicalDetails;
+    string ShippingWeight;
+    string ProductDimensions;
+    string Image;
+    string Variants;
+    string Sku;
+    string ProductUrl;
+    string Stock;
+    string ProductDetails;
+    string Dimensions;
+    string Color;
+    string Ingredients;
+    string Directions;
+    string Seller;
+    string SizeQuantityVariant;
+    string ProductDescription;
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
+#include "inventorylist.hpp"
 
 void printHelp()
 {
